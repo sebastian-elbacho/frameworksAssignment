02d651c03d934dc7d1814f77f48a888f9d1fd61c
SebastianK <sebastian.bachowski@gmail.com>
2025-11-25 13:40:34 +0000

    teams/forms and views created

---
+++ teams/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+    phone = forms.CharField(label='Telefon')
+    address = forms.CharField(label='Adres')
+    city = forms.CharField(label='Miasto')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie pasują do siebie!')
+        return cd['password2']
--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm
+from .models import UserProfile
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            UserProfile.objects.create(
+                user=new_user,
+                phone=form.cleaned_data['phone'],
+                address=form.cleaned_data['address'],
+                city=form.cleaned_data['city']
+            )
+            return redirect('login')  # tymczasowo – jeszcze nie mamy strony logowania
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'teams/register.html', {'form': form})
+
+
