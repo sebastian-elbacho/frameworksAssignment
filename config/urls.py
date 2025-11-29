f3539ebb684e11464147e12ca28e857baa1b37bc
SebastianK <sebastian.bachowski@gmail.com>
2025-11-29 16:19:04 +0000

    the registration button fixed, test ok

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -21,12 +22,14 @@ from teams import views as team_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teams/', include('teams.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='teams/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', team_views.home, name='home'),
-    path('teams/', include('teams.urls')),
+
+    # główna aplikacja
+    path('', include('teams.urls')),  
+
+    # login/logout
     path('login/', auth_views.LoginView.as_view(template_name='teams/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path('teams', include('teams.urls')),
+
+    # strona startowa
+    path('', team_views.home, name='home'),
 ]
